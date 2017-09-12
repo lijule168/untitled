a36045d62651c59475a632ef2efb1b2306a16ade
lijule168 <lijule168@163.com>
2017-09-12 20:00:04 +0800

    8888

---
+++ Test002/__init__.py
@@ -0,0 +1,52 @@
+# -- coding: utf-8 --
+print ("haha中国")
+#
+# for i in  range(1,5):
+#     for j in range(1,5):
+#         for k in  range(1,5):
+#             if(i!=k)and (i!=j)and (j!=k):
+#                 print i,j,k
+
+
+
+
+# for i in range(1,5):
+#     for j in range(0,6):
+#         print i+j;
+#
+#         print ("haha");
+#         print i;
+        # print j;
+
+
+
+
+# for(int i=1;i<=9;i++){
+# 		for(int j=1;j<=i;j++){
+# 		System.out.print(i + "*" + j + "=" + (i*j) + "\t");
+# 		}
+# 		System.out.println("\n");
+# 		}
+
+# for i in  range(1,10):
+#     for j in range(1,i+1):
+#         str=i +'*'+j+'=';
+#         print str +(i*j);
+#
+#     print '\n';
+
+# for i in range(1,10):
+#     for j in range(1,i+1):
+#         print("%d*%d=%2d" % (i,j,i*j),end=" ")
+#  print (" ")
+
+# for i in range(1,10):
+#     for j in range(1,i+1)
+#         print("%d*%d=%2d" % (i, j, i * j), '')
+#     print("");
+
+for i in range(1,10):
+    for j in range(1,i+1):
+        print j,'*',i,'=',i*j,' ',
+    if j==i:
+        print ''
